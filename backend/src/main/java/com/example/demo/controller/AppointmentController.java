6ca3020460c4bea2e502e06fca4e1d92dfac717e
rohit bansal <89240022+Rohitbansaldeveloper@users.noreply.github.com>
2026-05-17 17:14:33 +0530

    Update AppointmentController.java

--- backend/src/main/java/com/example/demo/controller/AppointmentController.java
+++ backend/src/main/java/com/example/demo/controller/AppointmentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//rohit
 @RestController
 @RequestMapping("/api/appointments")
 @CrossOrigin(origins = "*")
